c3295cc674ebc0ae96221adf603509cbb1b671e8
999 <999@hyeon.pro>
2023-05-14 09:02:04 +0000

    feat: 메세지 내용 수정

--- tvdb/obj2list.ts
+++ tvdb/obj2list.ts
@@ -5,23 +5,12 @@ import * as O from "https://deno.land/x/fp_ts@v2.11.4/Option.ts";
 
 
 function obj2list(obj: Record<string, any>) {
-  const template = (key, value) => `**${key}**\n${value}`
+  const template = (key, value) => `${key}\n${value}`
   return pipe(
-    Object.entries(obj),
-    A.filter(([key]) => key !== 'thumbnail'),
+    obj,
+    R.toArray,
     A.filter(([key]) => key !== 'overview'),
     A.map(([key, value]) => template(key, value)),
-    pairs => [
-      ...pairs,
-      pipe(
-        R.lookup('thumbnail', obj),
-        O.fold(
-          // onEmpty
-          () => undefined,
-          item => template('thumbnail', item),
-        ),
-      ), 
-    ],
     list => list.join('\n\n')
   )
 }
